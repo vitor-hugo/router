e56ad4b505ae991d2fec77fa1b2d5da96d843afe
vitor-hugo <vitor83dev@gmail.com>
2024-05-29 10:22:17 -0300

    init: normalizing and validating an URI

---
+++ src/Traits/UriTrait.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Router\Traits;
+
+trait UriTrait
+{
+    /**
+     * Is expected that the URI starts with '/' and not finishes with '/'
+     *
+     * Examples:
+     *   - '/user/e4a1e132'
+     *   - '/v1/user/0d5d6561'
+     *
+     * If the URI is 'user/902e1019/', will be normalized to '/user/902e1019'
+     *
+     * @param string $uri
+     * @return string Normalized URI
+     */
+    private function normalizeUri(string $uri): string
+    {
+        $uri = rtrim(trim($uri), "/");
+
+        if (!str_starts_with($uri, "/")) {
+            $uri = "/" . $uri;
+        }
+
+        return $uri;
+    }
+
+    /**
+     * Validates if the URI has unexpected characters like GET variables.
+     *
+     * Example of invalid uri is '/user?id=54509acf', is expected to be something like '/user/54509acf'.
+     *
+     * @param string $uri
+     * @return bool
+     */
+    private function validateUri(string $uri): bool
+    {
+        if (empty($uri)) {
+            return true;
+        }
+
+        if (@preg_match('/^[a-zA-Z0-9\/\-\.]*$/', $uri) == false) {
+            return false;
+        }
+
+        return true;
+    }
+}
