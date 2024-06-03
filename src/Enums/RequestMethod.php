1c03f9aed93d13996e9361a804f1ef6c42ddcd06
vitor-hugo <vitor83dev@gmail.com>
2024-06-03 11:21:33 -0300

    init: RequestMethod enum

---
+++ src/Enums/RequestMethod.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Router\Enums;
+
+use Torugo\Router\Exceptions\InvalidRequestMethod;
+
+enum RequestMethod: string
+{
+    case DELETE = 'delete';
+    case GET = 'get';
+    case PATCH = 'patch';
+    case POST = 'post';
+    case PUT = 'put';
+
+    public static function tryFromRequest(): static
+    {
+        $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
+        $case = self::tryFrom($requestMethod);
+
+        if (!$case) {
+            throw new InvalidRequestMethod("Invalid request method '$requestMethod'");
+        }
+
+        return $case;
+    }
+}
