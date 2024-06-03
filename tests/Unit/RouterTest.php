d1635cc13136fcda2e0e87cb8430ebfdcab0fbbc
vitor-hugo <vitor83dev@gmail.com>
2024-06-03 16:23:46 -0300

    add: tests of routes resolving

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Router\Unit;
 
+use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Tests\Router\Unit\Contracts\ValidControllerContract;
+use Torugo\Router\Enums\RequestMethod;
 use Torugo\Router\Exceptions\InvalidRouteException;
 use Torugo\Router\Router;
 
@@ -38,5 +40,60 @@ class RouterTest extends TestCase
         $this->router->register(ValidControllerContract::class);
         $this->assertCount(5, $this->router->getRoutes());
         return $this->router;
+    }
+
+    #[Depends("testControllerRegistration")]
+    #[TestDox("Resolving GET Requests")]
+    public function testResolveGetRequest(Router $router)
+    {
+        $response = $router->resolve('/users/234923', RequestMethod::GET);
+        $this->assertEquals("Returns a user with id '234923'.", $response);
+
+        $response = $router->resolve('/users', RequestMethod::GET);
+        $this->assertEquals("Returns all users.", $response);
+    }
+
+    #[Depends("testControllerRegistration")]
+    #[TestDox("Resolving POST Requests")]
+    public function testResolvePostRequest(Router $router)
+    {
+        $response = $router->resolve('/users', RequestMethod::POST);
+        $this->assertEquals("Adds a new user.", $response);
+
+        $response = $router->resolve('/users/search', RequestMethod::POST);
+        $this->assertEquals("Returns a filtered list of users.", $response);
+    }
+
+    #[Depends("testControllerRegistration")]
+    #[TestDox("Resolving PUT Requests")]
+    public function testResolvePutRequest(Router $router)
+    {
+        $response = $router->resolve('/users/2384789', RequestMethod::PUT);
+        $this->assertEquals("Updates the user id '2384789'.", $response);
+    }
+
+    #[Depends("testControllerRegistration")]
+    #[TestDox("Resolving PATCH Requests")]
+    public function testResolvePatchRequest(Router $router)
+    {
+        $response = $router->resolve('/users/status/23592783', RequestMethod::PATCH);
+        $this->assertEquals("Changes the status of user '23592783'.", $response);
+    }
+
+    #[Depends("testControllerRegistration")]
+    #[TestDox("Resolving DELETE Requests")]
+    public function testResolveDELETEhRequest(Router $router)
+    {
+        $response = $router->resolve('/users/234829', RequestMethod::DELETE);
+        $this->assertEquals("Deactivates the user with id '234829'.", $response);
+    }
+
+    #[Depends("testControllerRegistration")]
+    #[TestDox("Should Throw InvalidRouteException when route not found.")]
+    public function testShouldThrowWhenRouteNotFound(Router $router)
+    {
+        $this->expectException(InvalidRouteException::class);
+        $this->expectExceptionMessage("Route '/users/not/exists' not found.");
+        $router->resolve('/users/not/exists', RequestMethod::GET);
     }
 }
