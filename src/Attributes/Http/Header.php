a263317dd768d2f287f60952abe179f76a2a952b
vitor-hugo <vitor83dev@gmail.com>
2024-06-04 16:12:21 -0300

    init: Header attribute

---
+++ src/Attributes/Http/Header.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Router\Attributes\Http;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Header
+{
+    /**
+     * Defines a HTTP header
+     * @param string $param Http Parameter
+     * @param string $value Parameter's value
+     */
+    public function __construct(public string $param, public string $value)
+    {
+    }
+
+    /**
+     * Get the header as string
+     * @return string
+     */
+    public function toString(): string
+    {
+        $this->param = rtrim($this->param, ": ");
+        return "{$this->param}: {$this->value}";
+    }
+}
