c2765dd90d8ede47c3f017a73f85f9a61acf9bda
vitor-hugo <vitor83dev@gmail.com>
2024-06-04 16:07:31 -0300

    init: HttpCode attribute

---
+++ src/Attributes/Http/HttpCode.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Router\Attributes\Http;
+
+use Attribute;
+use Torugo\Router\Exceptions\InvalidResponseException;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class HttpCode
+{
+    /**
+     * Defines the HTTP status code for success responses
+     * @param int $code Valid HTTP status code [100..599]
+     */
+    public function __construct(public int $code)
+    {
+        $this->validateHttpCode($this->code);
+    }
+
+    /**
+     * Validates the HTTP Code
+     * @param int $code
+     * @throws \Torugo\Router\Exceptions\InvalidResponseException
+     * @return void
+     */
+    public function validateHttpCode(int $code): void
+    {
+        if ($code < 100 || $code > 599) {
+            throw new InvalidResponseException("The HTTP status code '$code' is invalid.", 1);
+        }
+    }
+}
