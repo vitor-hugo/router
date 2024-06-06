60e685074cace05704a8c2c12932a872624b8e2c
vitor-hugo <vitor83dev@gmail.com>
2024-06-06 09:28:22 -0300

    refact: adding http status code

--- src/Attributes/Http/Redirect.php
+++ src/Attributes/Http/Redirect.php
@@ -7,7 +7,21 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class Redirect
 {
-    public function __construct(public string $url)
+    /**
+     * Redirects a request to a defined URL
+     * @param string $url Redirect location
+     * @param int $statusCode Http Status Code, accepts only 301, 302, 307 or 308. Default is 301.
+     */
+    public function __construct(public string $url, public int $statusCode = 301)
     {
+        $this->checkHttpCode();
+    }
+
+    private function checkHttpCode(): void
+    {
+        $accepted = [301, 302, 307, 308];
+        if (!in_array($this->statusCode, $accepted, true)) {
+            $this->statusCode = 301;
+        }
     }
 }
