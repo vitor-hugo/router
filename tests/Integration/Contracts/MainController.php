6fcadb7b19dc2fee5a7311c76d15ac8a383242c0
vitor-hugo <vitor83dev@gmail.com>
2024-06-06 17:02:49 -0300

    add: integration tests with http requests

---
+++ tests/Integration/Contracts/MainController.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Router\Integration\Contracts;
+
+use Torugo\Router\Attributes\Http\Header;
+use Torugo\Router\Attributes\Http\Redirect;
+use Torugo\Router\Attributes\Request\Controller;
+use Torugo\Router\Attributes\Request\Delete;
+use Torugo\Router\Attributes\Request\Get;
+use Torugo\Router\Attributes\Request\Patch;
+use Torugo\Router\Attributes\Request\Post;
+use Torugo\Router\Attributes\Request\Put;
+
+#[Controller()]
+class MainController
+{
+    #[Get()]
+    #[Post()]
+    #[Put()]
+    #[Patch()]
+    #[Delete()]
+    #[Redirect("/index", 301)]
+    public function main()
+    {
+    }
+
+    #[Get("/index")]
+    #[Header("Content-Type", "text/plain;charset=UTF-8")]
+    public function index()
+    {
+        return "index";
+    }
+
+    #[Get("/google")]
+    #[Redirect("https://google.com", 302)]
+    public function redirectToGoole()
+    {
+    }
+}
---
+++ tests/Integration/IntegrationTest.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Router\Integration;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+use Tests\Router\Integration\Contracts\CostumersController;
+use Tests\Router\Integration\Contracts\MainController;
+use Tests\Router\Integration\Contracts\UsersController;
+use Torugo\Router\Router;
+
+#[TestDox("Integration Tests")]
+#[Group("Integration")]
+class IntegrationTest extends TestCase
+{
+    private Client $client;
+
+    public function setUp(): void
+    {
+        $this->client = new Client(["base_uri" => "http://localhost:8000"]);
+    }
+
+    #[TestDox("Should redirect to '/index'")]
+    public function testInsideRedirection()
+    {
+        $response = $this->client->request('GET', '/', ['allow_redirects' => true]);
+        $body = $response->getBody()->getContents();
+        $statusCode = $response->getStatusCode();
+        $header = $response->getHeader("Content-Type")[0];
+
+        $this->assertEquals("index", $body);
+        $this->assertEquals(200, $statusCode);
+        $this->assertEquals("text/plain;charset=UTF-8", $header);
+    }
+
+    #[TestDox("Should redirect to a external URL")]
+    public function testOutsideRedirection()
+    {
+        $response = $this->client->request('GET', '/google', ['allow_redirects' => false]);
+        $header = $response->getHeader('Location')[0];
+        $statusCode = $response->getStatusCode();
+        $this->assertEquals("https://google.com", $header);
+        $this->assertEquals(302, $statusCode);
+    }
+
+    #[TestDox("Should perform a GET request")]
+    public function testGetRequest(): void
+    {
+        $response = $this->client->request('GET', '/users');
+
+        $header = $response->getHeader("Content-Type")[0];
+        $this->assertEquals("application/json; charset=utf-8", $header);
+
+        $body = $response->getBody()->getContents();
+        $json = json_decode($body, true);
+        $this->assertIsArray($json);
+        $this->assertArrayHasKey("data", $json);
+        $this->assertCount(7, $json["data"]);
+    }
+
+    #[TestDox("Should perform a POST request")]
+    public function testPostRequest(): void
+    {
+        $payload = [
+            "id" => "8",
+            "email" => "test8@email.com",
+            "name" => "Test8"
+        ];
+
+        $response = $this->client->post('/users', ['form_params' => $payload]);
+        $body = $response->getBody()->getContents();
+        $json = json_decode($body, true);
+        $this->assertArrayHasKey("data", $json);
+        $this->assertSame($json["data"], $payload);
+    }
+
+    #[TestDox("Should perform a PUT request")]
+    public function testPutRequest(): void
+    {
+        $payload = [
+            "email" => "user7@email.com",
+            "name" => "User7"
+        ];
+
+        $response = $this->client->put('/users/7', ['form_params' => $payload]);
+        $body = $response->getBody()->getContents();
+        $json = json_decode($body, true);
+        $this->assertArrayHasKey("data", $json);
+        $this->assertArrayIsIdenticalToArrayIgnoringListOfKeys($json["data"], $payload, ["id"]);
+    }
+
+    #[TestDox("Should perform a PATCH request")]
+    public function testPatchRequest(): void
+    {
+        $payload = [
+            "email" => "user7@email.com",
+            "name" => "User7"
+        ];
+
+        $response = $this->client->patch('/users/7', ['form_params' => $payload]);
+        $body = $response->getBody()->getContents();
+        $json = json_decode($body, true);
+        $this->assertArrayHasKey("data", $json);
+        $this->assertArrayIsIdenticalToArrayIgnoringListOfKeys($json["data"], $payload, ["id"]);
+    }
+
+    #[TestDox("Should perform a DELETE request")]
+    public function testDeleteRequest(): void
+    {
+        $response = $this->client->delete('/users/3');
+        $body = $response->getBody()->getContents();
+        $json = json_decode($body, true);
+        $this->assertArrayHasKey("data", $json);
+        $search = array_search("3", array_column($json["data"], "id"));
+        $this->assertFalse($search);
+    }
+}
