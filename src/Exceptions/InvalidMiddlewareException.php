8cdebcae2c3c41706cc76854ee7d5ffa9a94839e
vitor-hugo <vitor83dev@gmail.com>
2024-06-11 15:50:24 -0300

    init: exception for invalid middleware class

---
+++ src/Exceptions/InvalidMiddlewareException.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Router\Exceptions;
+
+use Exception;
+use Throwable;
+
+class InvalidMiddlewareException extends Exception
+{
+    protected $message;
+    protected $code;
+
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        $this->message = $message;
+        $this->code = $code;
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString(): string
+    {
+        if ($this->code > 0) {
+            return "{$this->message} ({$this->code})";
+        } else {
+            return $this->message;
+        }
+    }
+}
