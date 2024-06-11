caf8900d882ec7cbc2250747f21ce1048532944c
vitor-hugo <vitor83dev@gmail.com>
2024-06-11 15:50:39 -0300

    init: Middleware attribute

---
+++ src/Attributes/Middleware.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Router\Attributes;
+
+use Attribute;
+use Torugo\Router\Exceptions\InvalidMiddlewareException;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+class Middleware
+{
+    public function __construct(
+        public string $middleware,
+        public string $method,
+        public array $args = []
+    ) {
+        $this->validateMiddlewareClass();
+    }
+
+    private function validateMiddlewareClass()
+    {
+        if (!class_exists($this->middleware)) {
+            throw new InvalidMiddlewareException("Middleware '{$this->getMiddlewareName()}' not found.", 1);
+        }
+    }
+
+    private function validateMiddlewareMethod()
+    {
+        if (!method_exists($this->middleware, $this->method)) {
+            throw new InvalidMiddlewareException("The method '$this->method' does not exists in '{$this->getMiddlewareName()}'", 2);
+        }
+    }
+
+    private function getMiddlewareName(): string
+    {
+        $arr = explode("/", $this->middleware);
+        return end($arr);
+    }
+}
