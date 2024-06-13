e15c108b4bc241f433c36ae23b0cfa98981deffc
vitor-hugo <vitor83dev@gmail.com>
2024-06-13 11:35:56 -0300

    fix: calling validateMiddlewareMethod on constructor

--- src/Attributes/Middleware.php
+++ src/Attributes/Middleware.php
@@ -14,6 +14,7 @@ class Middleware
         public array $args = []
     ) {
         $this->validateMiddlewareClass();
+        $this->validateMiddlewareMethod();
     }
 
     private function validateMiddlewareClass()
