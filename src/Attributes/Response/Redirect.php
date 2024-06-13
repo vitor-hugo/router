5f9f226db38637b7e5bd6526d0d430f8d86c0be2
vitor-hugo <vitor83dev@gmail.com>
2024-06-13 07:43:04 -0300

    refact: Redirect attribute now throws exception on invalid status code

--- src/Attributes/Response/Redirect.php
+++ src/Attributes/Response/Redirect.php
@@ -3,6 +3,7 @@
 namespace Torugo\Router\Attributes\Response;
 
 use Attribute;
+use Torugo\Router\Exceptions\InvalidResponseException;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Redirect
@@ -10,7 +11,7 @@ class Redirect
     /**
      * Redirects a request to a defined URL
      * @param string $url Redirect location
-     * @param int $statusCode Http Status Code, accepts only 301, 302, 307 or 308. Default is 301.
+     * @param int $statusCode Http Status Code, accepts only 300, 301, 302, 303, 304, 307 or 308. Default is 301.
      */
     public function __construct(public string $url, public int $statusCode = 301)
     {
@@ -19,9 +20,9 @@ class Redirect
 
     private function checkHttpCode(): void
     {
-        $accepted = [301, 302, 307, 308];
+        $accepted = [300, 301, 302, 303, 304, 307, 308];
         if (!in_array($this->statusCode, $accepted, true)) {
-            $this->statusCode = 301;
+            throw new InvalidResponseException("The redirection status code '{$this->statusCode}' is invalid. Please, check documentation.", 1);
         }
     }
 }
